a828245a72aaf42a8bbcb40a85ec39894e9482e3
ATX900 <benjie.work@gmail.com>
2017-11-06 01:49:16 +0800

    javascript standard style
    Implemented the JavaScript standard 'code' style after implementing the linter-js-standard package

--- scripts/main.js
+++ scripts/main.js
@@ -2,39 +2,39 @@
 // Implemented in Vanilla JavaScrtip with CORS (Cross-Origin Resource Sharing) workaround
 // Workaround from Ignat Ospadov (source: https://medium.com/@iospadov/when-cors-got-your-json-down-61de617aa7db)
 
-document.getElementById('getQuote').addEventListener('click', getQuote);
-document.getElementById('tweetQuote').addEventListener('click', tweetQuote);
+document.getElementById('getQuote').addEventListener('click', getQuote)
+document.getElementById('tweetQuote').addEventListener('click', tweetQuote)
 
-var jsonpData = [];
-var author = '';
-var quote = '';
+var jsonpData = []
+var author = ''
+var quote = ''
 
-function getQuote() {
-  var script = document.createElement("script");
-  script.src = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=displayData";
-  document.getElementsByTagName("head")[0].appendChild(script);
+function getQuote () {
+  var script = document.createElement('script')
+  script.src = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=displayData'
+  document.getElementsByTagName('head')[0].appendChild(script)
 }
 
-function displayData(response) {
-  jsonpData.push(response);
-  quote = response.quoteText;
-  author = response.quoteAuthor;
+function displayData (response) {                                     // called from the last line of the script.src (line 14)
+  jsonpData.push(response)
+  quote = response.quoteText
+  author = response.quoteAuthor
 
-  document.getElementById('quote').innerHTML = quote;
+  document.getElementById('quote').innerHTML = quote
 
   if (author) {
-    document.getElementById('author').innerHTML = 'By: ' + author;
+    document.getElementById('author').innerHTML = 'By: ' + author
   } else {
-    author = 'Unknown';
-    document.getElementById('author').innerHTML = 'By: ' + author;
+    author = 'Unknown'
+    document.getElementById('author').innerHTML = 'By: ' + author
   }
 }
 
-function tweetQuote() {
-  if (quote == '') {
+function tweetQuote () {
+  if (quote === '') {
     quote = 'Oops! You did not click the "Get Quote" button...'
-    document.getElementById('quote').innerHTML = quote;
+    document.getElementById('quote').innerHTML = quote
   } else {
-    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote) + '  -- ' + encodeURIComponent(author));
+    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote) + '  -- ' + encodeURIComponent(author))
   }
 }
