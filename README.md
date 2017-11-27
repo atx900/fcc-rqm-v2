d3b196e9b3f74fe64211e357d097287516217559
ATX900 <benjie.work@gmail.com>
2017-11-27 20:56:14 +0800

    Updated code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Random Quote Machine v2
-- A Free Code Camp front-end challenge, randomly generates a quote from Formastic API and provides the user an option to post the said quote on his/her Twitter account.
-- Updated version, implemented in Vanilla JavaScript with CORS (Cross-Origin Resource Sharing) workaround.
+Random Quote Machine v2.1
+- Free Code Camp front-end challenge, randomly generate a random quote using Formasitc API and provides an option to the quote message in Twitter.
+- v2.0 update: Implemented in vanilla JavaScript with Cross-Origin Resource Sharing (CORS) workaround learned from Ignat Ospadov (src: https://medium.com/@iospadov/when-cors-got-your-json-down-61de617aa7db)
+- v2.1: Cleaned up HTML code and included a favicon.ico
+- v2.1: Cleaned up vanilla JavaScript code and comments
 
 Resources used:
 - Bootstrap CSS
---
+++ scripts/quote.js
@@ -0,0 +1,33 @@
+var jsonpData = []                                                            // stores JSON-format generated quote, CORS workaround
+var quoteMessage = ''                                                         // stores random quote
+var quoteAuthor = ''                                                          // stores random quote author
+
+document.getElementById('getQuote').addEventListener('click', getQuote)
+document.getElementById('tweetQuote').addEventListener('click', tweetQuote)
+
+getQuote()                                                                    // generate random quote on page load or reload
+
+function getQuote () {                                                        // generate random quote
+  var script = document.createElement('script')
+  script.src = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=displayData'
+  document.getElementsByTagName('head')[0].appendChild(script)
+}
+
+function displayData (response) {                                             // called from the 'script' variable, CORS workaround
+  jsonpData.push(response)
+  quoteMessage = response.quoteText                                           // get quote message
+  quoteAuthor = response.quoteAuthor                                          // get quote author
+
+  document.getElementById('quote-message').innerHTML = quoteMessage
+
+  if (quoteAuthor) {
+    document.getElementById('quote-author').innerHTML = 'By: ' + quoteAuthor
+  } else {
+    quoteAuthor = 'Unknown'
+    document.getElementById('quote-author').innerHTML = 'By: ' + quoteAuthor
+  }
+}
+
+function tweetQuote () {                                                      // access Twitter API & paste quote message & author
+  window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quoteMessage) + '  -- ' + encodeURIComponent(quoteAuthor))
+}
